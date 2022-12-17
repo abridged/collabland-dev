e79a0493f9214c3ebe113f5e8c9a430ce2f58734
kenny-io <ekeneeze3@gmail.com>
2022-12-17 14:39:36 +0400

    added CTAs for consumer and contributor docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Collab.Land Tutorial - 5min ⏱️
+            className="button button--secondary button--lg margin-horiz--md margin-vert--md"
+            to="/docs/downstream-integrations/"
+          >
+            Build with Collab.Land APIs
+          </Link>
+          <Link
+            style={{ background: "#277148" }}
+            // can't find where the "button--secondary, button--lg etc" classes are defined or are inherited from. So I had to add the style here to enforce a darker green background by default.
+            className="button button--lg "
+            to="/docs/upstream-integrations/"
+          >
+            Contribute to Collab.Land
           </Link>
         </div>
       </div>
@@ -26,11 +35,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Collab.Land Developer Portal">
+      description="Collab.Land Developer Portal"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
