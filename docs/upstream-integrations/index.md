df775bb67b576d0b225e8a0caa0b11658eadace2
Ekene Eze <ekeneeze3@gmail.com>
2023-01-05 17:00:38 +0400

    Add Personas CTAs to docs homepage (#35)
    * added CTAs for consumer and contributor docs
    
    * minor updates to tgr tutorial
    
    * fix broken link and minor changes

--- docs/upstream-integrations/index.md
+++ docs/upstream-integrations/index.md
@@ -4,13 +4,15 @@ sidebar_position: 4
 
 # Extending Collab.Land
 
-## Add new Discord bot commands
+In this section, you'll Learn all you need to know to extend Collab.Land with your custom integrations and build apps for the Collab.Land ecosystem, and [marketplace](#).
+
+<!-- ## Add new Discord bot commands
 
 ## Add new wallet connections
 
 ## Connect to a new blockchain
 
-## Introduce a new governing asset
+## Introduce a new governing asset -->
 
 ```mdx-code-block
 import DocCardList from '@theme/DocCardList';
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,14 +4,14 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to connect and collaborate',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to connect and collaborate",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
         Collab.Land was designed from the ground up to facilitate connections
@@ -20,34 +20,33 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Create a community of communities',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Create a community of communities",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Collab.Land creates a community of communities to empower membership
-        and ownership through decentralized identities, crypto assets and 
-        verifiable credentials.
+        Collab.Land creates a community of communities to empower membership and
+        ownership through decentralized identities, crypto assets and verifiable
+        credentials.
       </>
     ),
   },
   {
-    title: 'Build a decentralized platform',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Build a decentralized platform",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Collab.Land promotes progressive decentralization to so that 
-        our communities and members can decide what data to own 
-        and what to share.
-        Collab.Land is in the process of opening its source code and platform 
-        to developers and exit to the community for ownership and governance.
+        Collab.Land promotes progressive decentralization to so that our
+        communities and members can decide what data to own and what to share.
+        Collab.Land is in the process of opening its source code and platform to
+        developers and exit to the community for ownership and governance.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
