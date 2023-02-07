3b4a7950e47b17821da4dd82e9d51148fcb063f2
Ekene Eze <ekeneeze3@gmail.com>
2023-02-07 15:40:48 +0400

    Improve Hero Responsiveness (#45)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className='container'>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
         <div className={styles.header}>
           <div className={styles.content}>
             <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
-            <p className={clsx('hero__subtitle', styles.subtitle)}>{siteConfig.tagline}</p>
+            <p className={clsx("hero__subtitle", styles.subtitle)}>
+              {siteConfig.tagline}
+            </p>
             <div className={styles.button}>
-              <Link
-                className={styles.buttonContent}
-                to='/docs/intro'>
-                  Get Started 
-                  <img width={40} height={40} src={require('@site/static/img/point.png').default} />
+              <Link className={styles.buttonContent} to="/docs/intro">
+                Get Started
+                <img
+                  width={40}
+                  height={40}
+                  loading="eager"
+                  src={require("@site/static/img/point.png").default}
+                />
               </Link>
             </div>
           </div>
           <div>
-            <img width={500} height={600} src='https://res.cloudinary.com/collabland/image/upload/f_auto,q_auto/v1673350481/robotmascot.png' />
-         </div>
+            <img
+              loading="eager"
+              width={500}
+              height={600}
+              src="https://res.cloudinary.com/collabland/image/upload/f_auto,q_auto/v1673350481/robotmascot.png"
+            />
+          </div>
         </div>
       </div>
     </header>
@@ -34,11 +44,9 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Collab.Land Docs">
+    <Layout title={`${siteConfig.title}`} description="Collab.Land Docs">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
