9c178c5dd43a56425f88509788729a6520afb0bb
CryptoChibs <chibi@collab.land>
2025-04-10 17:59:02 -0300

    refactor(HomepageFeatures): Clear out feature list and update image handling
    - Removed all feature items from the FeatureList.
    - Updated image rendering logic to conditionally display images based on availability.

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,73 +10,21 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
-  // add a button to all of these items
-  {
-    title: 'Developer Portal',
-    ImageURL:
-      'https://res.cloudinary.com/collabland/image/upload/f_auto,q_auto/v1673350407/tools.webp',
-    description: (
-      <>
-        Access your developer dashboard and perform more actions like creating
-        client apps, generating API credentials and more...
-      </>
-    ),
-    button: (
-      <div className={styles.button}>
-        <Link
-          className={styles.buttonContent}
-          to="https://dev-portal.collab.land/"
-        >
-          Developers
-        </Link>
-      </div>
-    ),
-  },
-  {
-    title: 'Easy to connect and collaborate',
-    ImageURL:
-      'https://res.cloudinary.com/collabland/image/upload/f_auto,q_auto/v1673350408/globe.webp',
-    description: (
-      <>
-        Collab.Land integrates popular web2 platforms with web3 infrastures to
-        facilitate connections and collaborations across the internet.
-      </>
-    ),
-    // button: 'Get Started',
-    button: (
-      <div className={styles.button}>
-        <Link className={styles.buttonContent} to="/help-docs/intro">
-          Members
-        </Link>
-      </div>
-    ),
-  },
-
-  {
-    title: 'Create a community of communities',
-    ImageURL:
-      'https://res.cloudinary.com/collabland/image/upload/f_auto,q_auto/v1673350407/rocket.webp',
-    description: (
-      <>
-        Using decentralized identities, crypto assets, and verifiable
-        credentials, Collab.Land is creating a community of communities.
-      </>
-    ),
-    button: (
-      <div className={styles.button}>
-        <Link className={styles.buttonContent} to="/help-docs/intro">
-          Admins
-        </Link>
-      </div>
-    ),
-  },
+  // Feature list is now empty
 ];
 
 function Feature({ title, description, ImageURL, button }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img src={ImageURL} className={styles.featuredImg} role="Collab.Land" />
+        {ImageURL && (
+          <img
+            className={styles.featureSvg}
+            alt={title}
+            src={ImageURL}
+            role="img"
+          />
+        )}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
